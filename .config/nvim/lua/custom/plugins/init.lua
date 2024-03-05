7295dd01cd15a7a6a9eda4028787fd08f77a7a6e
Sam Wolfson <terabyte128@gmail.com>
2024-03-04 22:45:01 -0800

    fork solarized colorscheme  to make italic strings optional

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -198,16 +198,15 @@ return {
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'shaunsingh/solarized.nvim',
+    'terabyte128/solarized.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'solarized'
       vim.opt.background = 'light'
-
+      vim.cmd.colorscheme 'solarized'
       -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
