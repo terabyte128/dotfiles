0e279c8ac849f35d7798de0b2413cdd236ae8eeb
Sam Wolfson <terabyte128@gmail.com>
2024-03-12 22:07:36 -0700

    use treesitter as completion source

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -9,6 +9,7 @@
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
+
 return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
