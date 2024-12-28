d23bca9ae768a34aafe151f3aefdb974110011f3
Sam Wolfson <terabyte128@gmail.com>
2024-12-28 00:16:59 -0800

    .

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -50,34 +50,14 @@ return {
     cmd = 'Trouble',
     keys = {
       {
-        '<leader>xx',
+        '<leader>cD',
         '<cmd>Trouble diagnostics toggle<cr>',
-        desc = 'Diagnostics (Trouble)',
-      },
-      {
-        '<leader>xX',
-        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-        desc = 'Buffer Diagnostics (Trouble)',
+        desc = 'File Diagnostics',
       },
       {
         '<leader>cs',
         '<cmd>Trouble symbols toggle focus=false<cr>',
-        desc = 'Symbols (Trouble)',
-      },
-      {
-        '<leader>cl',
-        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-        desc = 'LSP Definitions / references / ... (Trouble)',
-      },
-      {
-        '<leader>xL',
-        '<cmd>Trouble loclist toggle<cr>',
-        desc = 'Location List (Trouble)',
-      },
-      {
-        '<leader>xQ',
-        '<cmd>Trouble qflist toggle<cr>',
-        desc = 'Quickfix List (Trouble)',
+        desc = 'Symbols',
       },
     },
   },
@@ -225,6 +205,7 @@ return {
         json = { 'prettier' },
         yaml = { 'prettier' },
         sh = { 'shfmt' },
+        swift = { 'swiftformat' },
       },
       formatters = {
         shfmt = {
