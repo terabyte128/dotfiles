220772142f4f20faed2e3ad9dcc086acffaad975
Sam Wolfson <terabyte128@gmail.com>
2025-04-29 19:20:07 -0700

    update nvim and wezterm

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -36,15 +36,54 @@ end
 return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   {
-    'OXY2DEV/markview.nvim',
-    lazy = false,
-
-    -- For blink.cmp's completion
-    -- source
-    -- dependencies = {
-    --     "saghen/blink.cmp"
-    -- },
-    opts = {},
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = 'gemini',
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
   },
   {
     'f-person/auto-dark-mode.nvim',
@@ -73,48 +112,48 @@ return {
       }
     end,
   },
-  {
-    'robitx/gp.nvim',
-    config = function()
-      local conf = {
-        providers = {
-          openai = {
-            disable = true,
-          },
-          copilot = {
-            disable = false,
-          },
-          googleai = {
-            disable = false,
-          },
-        },
-        agents = {
-          {
-            provider = 'googleai',
-            name = 'ChatGemini',
-            chat = true,
-            command = false,
-            -- string with model name or table with model name and parameters
-            model = { model = 'gemini-2.0-flash', temperature = 1.1, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = require('gp.defaults').chat_system_prompt,
-          },
-          {
-            provider = 'googleai',
-            name = 'CodeGemini',
-            chat = false,
-            command = true,
-            -- string with model name or table with model name and parameters
-            model = { model = 'gemini-2.0-flash', temperature = 0.8, top_p = 1 },
-            system_prompt = require('gp.defaults').code_system_prompt,
-          },
-        },
-      }
-      require('gp').setup(conf)
-
-      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
-    end,
-  },
+  -- {
+  --   'robitx/gp.nvim',
+  --   config = function()
+  --     local conf = {
+  --       providers = {
+  --         openai = {
+  --           disable = true,
+  --         },
+  --         copilot = {
+  --           disable = false,
+  --         },
+  --         googleai = {
+  --           disable = false,
+  --         },
+  --       },
+  --       agents = {
+  --         {
+  --           provider = 'googleai',
+  --           name = 'ChatGemini',
+  --           chat = true,
+  --           command = false,
+  --           -- string with model name or table with model name and parameters
+  --           model = { model = 'gemini-2.0-flash', temperature = 1.1, top_p = 1 },
+  --           -- system prompt (use this to specify the persona/role of the AI)
+  --           system_prompt = require('gp.defaults').chat_system_prompt,
+  --         },
+  --         {
+  --           provider = 'googleai',
+  --           name = 'CodeGemini',
+  --           chat = false,
+  --           command = true,
+  --           -- string with model name or table with model name and parameters
+  --           model = { model = 'gemini-2.0-flash', temperature = 0.8, top_p = 1 },
+  --           system_prompt = require('gp.defaults').code_system_prompt,
+  --         },
+  --       },
+  --     }
+  --     require('gp').setup(conf)
+  --
+  --     -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+  --   end,
+  -- },
   {
     'mbbill/undotree',
     keys = { -- load the plugin only when using it's keybinding:
