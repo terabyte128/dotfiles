b94fbc0679113ced287c304371e91ac98a5a57d9
Sam Wolfson <terabyte128@gmail.com>
2025-04-30 23:56:22 -0700

    EVERYTHING

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -36,35 +36,6 @@ end
 return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   {
-    'GeorgesAlkhouri/nvim-aider',
-    cmd = 'Aider',
-    -- Example key mappings for common actions:
-    keys = {
-      { '<leader>a/', '<cmd>Aider toggle<cr>', desc = 'Toggle Aider' },
-      { '<leader>as', '<cmd>Aider send<cr>', desc = 'Send to Aider', mode = { 'n', 'v' } },
-      { '<leader>ac', '<cmd>Aider command<cr>', desc = 'Aider Commands' },
-      { '<leader>ab', '<cmd>Aider buffer<cr>', desc = 'Send Buffer' },
-      { '<leader>a+', '<cmd>Aider add<cr>', desc = 'Add File' },
-      { '<leader>a-', '<cmd>Aider drop<cr>', desc = 'Drop File' },
-      { '<leader>ar', '<cmd>Aider add readonly<cr>', desc = 'Add Read-Only' },
-      { '<leader>aR', '<cmd>Aider reset<cr>', desc = 'Reset Session' },
-      -- Example nvim-tree.lua integration if needed
-      { '<leader>a+', '<cmd>AiderTreeAddFile<cr>', desc = 'Add File from Tree to Aider', ft = 'NvimTree' },
-      { '<leader>a-', '<cmd>AiderTreeDropFile<cr>', desc = 'Drop File from Tree from Aider', ft = 'NvimTree' },
-    },
-    dependencies = {
-      'folke/snacks.nvim',
-      --- The below dependencies are optional
-      'catppuccin/nvim',
-      'nvim-tree/nvim-tree.lua',
-      --- Neo-tree integration
-      'nvim-neo-tree/neo-tree.nvim',
-    },
-    config = function(opts)
-      require('nvim_aider').setup(opts)
-    end,
-  },
-  {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
     dependencies = {
@@ -81,7 +52,6 @@ return {
     },
     config = function(opts)
       require('neo-tree').setup(opts)
-      require('nvim_aider.neo_tree').setup(opts)
     end,
   },
   {
